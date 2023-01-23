cc9bc1c6f7bd757e50468715ea057b6bcfe20b9d
Price Hiller <price@orion-technologies.io>
2023-01-22 22:03:38 -0600

    Revert "refactor: no longer chown SQUADJS_DIR"
    This reverts commit 831e2305d5bdf9f8f2ee45b4b2a7f1933f79314a.

--- scripts/entry.bash
+++ scripts/entry.bash
@@ -32,6 +32,7 @@ main() {
 	done < "${SQUAD_SERVER_DIR}/SquadGame/ServerConfig/Rcon.cfg" > "rcon.temp" && mv "rcon.temp" "${SQUAD_SERVER_DIR}/SquadGame/ServerConfig/Rcon.cfg"
 
 	chown "${USER}:${USER}" "${SQUAD_SERVER_DIR}/SquadGame/ServerConfig/Rcon.cfg"
+	chown -R "${USER}:${USER}" "${SQUADJS_DIR}"
 
 	su "${USER}" - <<- __EOC__
 		printf "Starting the Squad Server....\n"
