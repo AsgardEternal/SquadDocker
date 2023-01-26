5841fe8fc9bf139d484ab704d3f5e7abc953abbf
Price Hiller <price@orion-technologies.io>
2023-01-25 20:39:44 -0600

    refactor: do not suppress squad log output

--- scripts/entry.bash
+++ scripts/entry.bash
@@ -34,7 +34,7 @@ main() {
 			Port="${GAMEPORT}" \
 			QueryPort="${QUERYPORT}" \
 			FIXEDMAXTICKRATE="${FIXEDMAXTICKRATE}" \
-			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" >/dev/null 2>&1 &
+			FIXEDMAXPLAYERS="${FIXEDMAXPLAYERS}" &
 		printf "Squad Server Started!\n"
 
 		wait
