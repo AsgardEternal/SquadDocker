4ba777edc2e43520ea602fd0da2bf1276850ec6d
Price Hiller <price@orion-technologies.io>
2024-11-10 19:03:14 -0600

    feat: add *hacky* support for "windows only" mods on Linux

--- scripts/entry.bash
+++ scripts/entry.bash
@@ -17,24 +17,53 @@ install-squad-server() {
 }
 
 install-squad-mod() {
-	local mod="${1}"
+	local mod_id="${1}"
+	local mod_path="${SQUAD_SERVER_DIR}/SquadGame/Plugins/Mods/${mod_id}"
 	su "${STEAM_USER}" - <<-__EOC__
-		printf "\n\n######\nAdding mod: %s\n######\n\n" "${mod}"
+		printf "\n\n######\nAdding mod: %s\n######\n\n" "${mod_id}"
 		counter=0
 		until "${STEAM_CMD_INSTALL_DIR}/steamcmd.sh" \
-		    +force_install_dir "${SQUAD_SERVER_DIR}/SquadGame/Plugins/Mods/${mod}" \
+		    +force_install_dir "${mod_path}" \
 		    +login anonymous \
-		    +workshop_download_item "${WORKSHOP_ID}" "${mod}" validate \
+		    +workshop_download_item "${WORKSHOP_ID}" "${mod_id}" validate \
 		    +quit; do
-		    printf "\nDid Not Fully Download %s, making another attempt.\n" "${mod}"
+		    printf "\nDid Not Fully Download %s, making another attempt.\n" "${mod_id}"
 		    (( counter++ ))
 		    if (( counter > 5 )); then
-		        printf "Critical failure, could not download the mod: %s\n" "${mod}"
+		        printf "Critical failure, could not download the mod: %s\n" "${mod_id}"
 		        exit 1
 		    fi
 		done
 	__EOC__
+	# The assignment magic below ensures the glob expansion works correctly
+	local _mod_paks_path=("${mod_path}/steamapps/workshop/content/${WORKSHOP_ID}/${mod_id}/"*"/Content/Paks")
+	local mod_paks_path="${_mod_paks_path[*]}"
+	local mod_paks_linux_path="${mod_paks_path}/LinuxServer"
+	local mod_paks_no_linux_support_f="${mod_paks_path}/no-linux-support"
+	local mod_paks_win_path="${mod_paks_path}/WindowsNoEditor"
+	printf "\nMod Paks path is: '%s'\n" "${mod_paks_path}"
 
+	if [[ -f "${mod_paks_no_linux_support_f}" ]]; then
+		rm -rf "${mod_paks_linux_path}"
+	fi
+
+	if  [[ ! -r "${mod_paks_linux_path}" ]] || [[ -h "${mod_paks_linux_path}" ]]; then
+		touch "${mod_paks_no_linux_support_f}"
+		cat <<-__EOS__
+			======================= WARN ========================
+			   Mod ${mod_id} may not be compatible with Linux!
+
+			      Attempting to link Windows Paks for Linux
+
+			                 THIS MAY NOT WORK!
+			=====================================================
+		__EOS__
+		printf "Linking '%s' -> '%s'\n" "${mod_paks_win_path}" "${mod_paks_linux_path}"
+		rm -f "${mod_paks_linux_path}" >/dev/null 2>&1 || true
+		cp -rf "${mod_paks_win_path}" "${mod_paks_linux_path}"
+		chown -R "${STEAM_USER}:${STEAM_USER}" "${mod_paks_linux_path}"
+		rm -rf "${mod_paks_win_path}"
+	fi
 }
 
 init-docker-mount() {
